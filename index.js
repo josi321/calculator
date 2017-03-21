f3fd08996feba80a58724f65352f09df6ef9bb0e
josi321 <josi_on83@hotmail.com>
2017-03-20 23:28:56 -0400

    #02: defining functions
    Added the function to calculate the cost of the project. A constant
    Calculator was created to contain the costOfProject function, which
    will be exported to the test.js file to test. I set up basic parameters
    for the function, such as checking to see if the base price/ input
    price is valid (if its not, then throw an error. if its a string, then
    parse it. if its not a number, throw an error). Made variables to
    calculate the cost, and returned it to the function as a float with 2
    decimal places. A function for calculating the markup per person and
    categories was also made. The category type used a switch-case
    function. I may change this to an object literal table to match with
    current trends of javascript (if time permits).

--- index.js
+++ index.js
@@ -5,3 +5,50 @@ const food = 0.13;
 const electronics = 0.02;
 const persons = 0.012;
 const defaultMarkup = 0;
+
+//function to calculate the cost of the project
+const Calculator = {
+
+  calculate(basePrice, quantity, category) {
+    //if basePrice is not a number, then parse it, and throw an error to ask for a baseprice as a float
+    if (isNaN(parseFloat(basePrice))) {
+      throw new Error(`${basePrice} is not a number and is invalid. It should be type float`);
+    }
+    //if the basePrice is type string, then parse it
+    if ( typeof basePrice === 'string' ) {
+      basePrice = parseFloat( basePrice );
+    } else if ( typeof basePrice !== 'number' ) {
+      throw new Error( 'basePrice should be float or string' );
+    }
+
+    //function to calculate the total cost
+    let flatMarkupBaseCost = basePrice * (1 + flat);
+    let personsCost = flatMarkupBaseCost + (flatMarkupBaseCost * this.personsMarkup(quantity))
+    let finalCost = personsCost + (flatMarkupBaseCost * this.categoryMarkup(category));
+    return Number(Math.round(finalCost+ 'e2') + 'e-2');
+  },
+
+  //setting up the category markup
+  categoryMarkup(category) {
+    switch (category) {
+      case 'drug': case 'drugs': case 'pharmaceuticals': case 'pharma':
+        return drug;
+      case 'food':
+        return food;
+      case 'electronics':
+        return electronics;
+      default:
+        return defaultMarkup;
+    }
+  },
+
+  //function to calculate the markup per worker. people variable to check to make sure that people # is valid
+  personsMarkup(workers) {
+    let people = parseInt(workers) === workers && workers >= 0;
+    return people ? workers * persons : 0;
+  }
+
+};
+
+//export this to the test.js file
+export default Calculator;
