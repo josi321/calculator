287df588b7c664abdaec52130befa97f8d4cbe30
josi321 <josi_on83@hotmail.com>
2017-03-20 23:54:42 -0400

    #03: set up test.js
    added the rest of the testing parameters, however, when I tried
    testing, there are some errors (syntax). I will need to fix this

--- test/test.js
+++ test/test.js
@@ -13,12 +13,6 @@ chai.should();
 
 describe('Calculator', () => {
   describe('calculate', () => {
-    it('should throw error if the basePrice is invalid', () => {
-      (function() {
-        Calculator.calculate('randomstring', 3, 'drugs');
-      }).should.throw(Error, /basePrice should be type float/);
-    });
-
     it('should produce $1,591.58 given $1,299.99, 3 people, food', () => {
       Calculator.calculate(1299.99, 3, 'food').should.equal(1591.58);
     });
@@ -30,4 +24,47 @@ describe('Calculator', () => {
     it('should produce $13,707.63 given $12,456.95, 4 people, books', () => {
       Calculator.calculate(12456.95, 4, 'books').should.equal(13707.63);
     });
-  }
+
+    it('should throw error if the basePrice is invalid', () => {
+      (function() {
+        Calculator.calculate('randomstring', 3, 'drugs');
+      }).should.throw(Error, /randomstring is invalid, it should be type float/);
+    });
+  }), //ends calculate
+
+  describe('categoryMarkup', () => {
+    it('should return 7.5% given category is drug or drugs, pharmaceuticals or pharma', () => {
+      Calculator.categoryMarkup('drug').should.equal(0.075);
+      Calculator.categoryMarkup('drugs').should.equal(0.075);
+      Calculator.categoryMarkup('pharmaceuticals').should.equal(0.075);
+    });
+
+    it('should return 13% given category is food', () => {
+      Calculator.categoryMarkup('food').should.equal(0.13);
+    });
+
+    it('should return 2% given category is electronics', () => {
+      Calculator.categoryMarkup('electronics').should.equal(0.02);
+    });
+
+    it('should return no markup if no category is given', () => {
+      Calculator.categoryMarkup().should.equal(0);
+      Calculator.categoryMarkup(null).should.equal(0);
+      Calculator.categoryMarkup(undefined).should.equal(0);
+      Calculator.categoryMarkup('foobar').should.equal(0);
+    });
+  }), //ends categoryMarkup
+
+  describe('personsMarkup', () => {
+    it('should return 1.2% per staff required', () => {
+      Calculator.personsMarkup(3).should.equal(0.012 * 3);
+    });
+
+    it('should return 0 if no staff given or invalid input', () => {
+      Calculator.personsMarkup(0).should.equal(0);
+      Calculator.personsMarkup('foobar').should.equal(0);
+      Calculator.personsMarkup(-5).should.equal(0);
+      Calculator.personsMarkup(2.6).should.equal(0);
+    });
+  }), //ends personsMarkup
+}); //ends the Calculator
