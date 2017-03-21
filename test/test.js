6a468dc7ccb102b1f624dbb3eedcfe2c6907fd14
josi321 <josi_on83@hotmail.com>
2017-03-20 23:45:47 -0400

    #03: set up test.js
    Add the test parameters for the 3 example inputs and outputs, as well
    as the throwing an error when base price is not a number/float.
    However, when i tried to run the test, it gave me some errors (says
    syntax errors), so I will need to fix that.

--- test/test.js
+++ test/test.js
@@ -10,3 +10,24 @@
 import Calculator from './';
 let chai = require('chai');
 chai.should();
+
+describe('Calculator', () => {
+  describe('calculate', () => {
+    it('should throw error if the basePrice is invalid', () => {
+      (function() {
+        Calculator.calculate('randomstring', 3, 'drugs');
+      }).should.throw(Error, /basePrice should be type float/);
+    });
+
+    it('should produce $1,591.58 given $1,299.99, 3 people, food', () => {
+      Calculator.calculate(1299.99, 3, 'food').should.equal(1591.58);
+    });
+
+    it('should produce $6,199.81 given $5,432.00, 1 person, drugs', () => {
+      Calculator.calculate(5432.00, 1, 'drugs').should.equal(6199.81);
+    });
+
+    it('should produce $13,707.63 given $12,456.95, 4 people, books', () => {
+      Calculator.calculate(12456.95, 4, 'books').should.equal(13707.63);
+    });
+  }
